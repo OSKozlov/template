acfc473581c16c33f13f5bd902529f5b06c8e0d2
Oleksandr Kozlov <Oleksandr.Kozlov@halliburton.com>
2019-02-11 20:37:55 +0200

    angular template example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, AfterViewInit, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'template';
+export class AppComponent implements AfterViewInit {
+  title = 'Template';
+
+  @ViewChild('someInput') someInput: ElementRef;
+
+  ngAfterViewInit() {
+  }
+
+  appendChild(): void {
+    if (this.someInput.nativeElement !== undefined && this.someInput.nativeElement !== null) {
+      this.someInput.nativeElement.value = 'Anchovies! 🍕🍕';
+    }
+  }
 }
